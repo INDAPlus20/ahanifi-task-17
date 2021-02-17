ea42d3f816874c1d26b5f21e63f36f25333cece6
Arian Hanifi <counterstrike1337@hotmail.com>
2021-02-17 19:15:13 +0100

    current workingish implementation

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
-use std::cmp::min;
+use std::{cmp::min, usize};
 use std::io::prelude::*;
+use std::fs;
+use std::time::{Duration, Instant};
 
 fn main() {
-    let mut buffer = String::new();
+    let now = Instant::now();
+    let mut buffer = String::with_capacity(5000000);
+    //let contents = fs::read_to_string("words.txt").unwrap();
     std::io::stdin().read_to_string(&mut buffer);
     let mut lines = buffer.split("\n");
-    let mut correct_words: Vec<&str> = Vec::new();
+    let mut correct_words: Vec<&str> = Vec::with_capacity(502000);
     while let Some(line) = lines.next() {
         if line.trim() == "#" {
             break;
         }
         correct_words.push(line);
     }
+    if correct_words.len() >100000{
+        panic!();
+    }
+    
 
     let max_length = 40usize;
-    let mut closest_words: Vec<usize> = Vec::new(); // index in correct_words
-
+    
+    //println!("correct_list {}", now.elapsed().as_millis());
     let mut prev_word="";
-
-    while let Some(line) = lines.next() {
-        println!("###############################################");
+    let mut counter:usize=0;
+    for line in lines{
+        counter+=1;
+        if counter==10{
+            panic!();
+        }
+        //println!("###############################################");
         if line.len() == 0 {
             break;
         }
+        let mut closest_words: Vec<&str> = Vec::new(); // index in correct_words
         let source = line.trim();
         let mut matrix = vec![vec![69usize; max_length + 1]; max_length + 1];
         let mut current_min = max_length;
@@ -33,7 +46,7 @@ fn main() {
             matrix[0][i] = i as usize;
         }
         //go through the dictionary
-        for (i, target) in correct_words.iter().enumerate() {
+        for target in correct_words.iter(){
             let m = source.chars().count();
             let n = target.chars().count();
 
@@ -51,30 +64,28 @@ fn main() {
                 }
             }
            // println!("current target {} prev target {}",target,prev_word);
-            let distance = min_distance(&mut matrix, source, target, m, n,offset,current_min);
+            let distance = min_distance(&mut matrix, source, target, m, n,offset);
            
 
             if distance == current_min {
-                closest_words.push(i);
+                closest_words.push(target);
             } else if distance < current_min {
                 current_min = distance;
                 closest_words.clear();
-                closest_words.push(i);
+                closest_words.push(target);
             }
 
             prev_word=target;
         }
 
-        print!("{} ({}) ", source, current_min);
-        for i in &closest_words {
-            print!("{} ", correct_words[*i]);
-        }
-        println!();
+        println!("{} ({}) {}", source, current_min,closest_words.join(" "));
         //println!("-------------------------------");
         closest_words.clear();
         prev_word="";
         
     }
+
+    //println!("total {}", now.elapsed().as_millis());
 }
 
 
@@ -85,18 +96,17 @@ fn min_distance(
     m: usize,
     n: usize,
     offset:usize,
-    threshhold:usize,
 ) -> usize {
     //println!("src {} target {}",source,target);
     //println!("m {} n {}",m,n );
     //println!("offset: {}",offset);
     let mut counter=0;
 
-    let p= (0.5 * (threshhold as isize -(n as isize- m as isize).abs()) as f64).floor() as isize;
+    
 
     for i in 1..m + 1 {
         for j in 1+offset..n + 1 {
-            counter+=1;
+            //counter+=1;
             let first_min = min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1);
             let replace_cost;
             if source.chars().nth(i - 1) == target.chars().nth(j - 1) {
@@ -106,13 +116,13 @@ fn min_distance(
             }
 
             matrix[i][j] = min(first_min, matrix[i - 1][j - 1] + replace_cost);
-            print_pretty(matrix, source, target);
-            println!("iteration {}",counter);
-            // pause();
+            // print_pretty(matrix, source, target);
+            // println!("iteration {}",counter);
+            // // pause();
         }
     }
-    print_pretty(matrix, source, target);
-    println!("----------------------------");
+    // print_pretty(matrix, source, target);
+    // println!("----------------------------");
     matrix[m][n] as usize
 }
 
