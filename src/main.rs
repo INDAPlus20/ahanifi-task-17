7ec39125edb5d116bfc564f7654547e15c10e6e1
Arian Hanifi <counterstrike1337@hotmail.com>
2021-02-19 13:38:50 +0100

    implement faster solution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::io::prelude::*;
+use std::{cmp::max, io::prelude::*};
 #[cfg(testing)]
 use std::time::{Duration, Instant};
 use std::{cmp::min, fmt, usize};
+
+const MAX_LENGTH:usize=40;
 
 #[derive(Clone, Copy)]
 struct CharArray {
@@ -71,13 +73,13 @@ fn main() {
     let mut correct_words: Vec<CharArray> = Vec::with_capacity(500000);
     //let mut misspelled_words: Vec<CharArray> = Vec::with_capacity(1000);
 
-    let mut bytes = Vec::with_capacity(5000000);
-    std::io::stdin().read_to_end(&mut bytes);
-    let mut byte_iter = bytes.iter();
+    // let mut bytes = Vec::with_capacity(5000000);
+    // std::io::stdin().read_to_end(&mut bytes);
+    // let mut byte_iter = bytes.iter();
 
-    // let mut input = String::new();
-    // std::io::stdin().read_to_string(&mut input);
-    // let mut byte_iter=input.bytes();
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input);
+    let mut byte_iter=input.bytes();
 
     let mut word_buffer = CharArray::new();
 
@@ -97,21 +99,21 @@ fn main() {
                 0xb6 => 0x7d, // ö => }
                 _ => panic!("non-allowed character!"),
             }),
-            b => word_buffer.push(*b),
+            b => word_buffer.push(b),
         }
     }
 
-    let max_length = 40usize;
-
     let mut prev_word: CharArray = CharArray::new();
     let mut closest_words: Vec<CharArray> = Vec::with_capacity(128);
-    let mut matrix = vec![vec![0; max_length + 1]; max_length + 1];
+
+    let mut matrix = [[99; MAX_LENGTH + 1]; MAX_LENGTH + 1];
 
     //initialize basecases, i.e the margins
-    for i in 0..max_length + 1 {
+    for i in 0..MAX_LENGTH + 1 {
         matrix[i][0] = i;
         matrix[0][i] = i;
     }
+    
 
     while let Some(byte) = byte_iter.next() {
         // println!("byte {}",byte);
@@ -119,15 +121,13 @@ fn main() {
         match byte {
             b'\n' | b'\r' => { //for every misspelled word
 
-                let mut current_min = max_length;
-                let m = word_buffer.len;
-
-                
+                let mut current_min = MAX_LENGTH;
+                let m = word_buffer.len();
                 
                 //go through the dictionary
 
                 for target in correct_words.iter() {
-                    let n = target.len;
+                    let n = target.len();
 
                     let diff = if m >= n { m - n } else { n - m };
 
@@ -137,29 +137,26 @@ fn main() {
 
                     let offset= prev_word.similar(target);
                     
-
-                    for i in 1..m + 1 {
-                        for j in 1 + offset..n + 1 {
-
-                            let replace_cost =
-                                if word_buffer.bytes[(i - 1)] == target.bytes[(j - 1)] {
-                                    0
-                                } else {
-                                    1
-                                };
-
-                            let ins_del_minimum = min(matrix[(i - 1)][j] + 1, matrix[i][j - 1] + 1);
-                            matrix[i][j] =
-                                min(ins_del_minimum, matrix[i - 1][j - 1] + replace_cost);
-                        }
-                    }
+                    //let minlen = min(prev_word.len, n);
+                    // let mut offset=minlen;
+                    // for i in 0..minlen {
+                    //     if prev_word.bytes[i] != target.bytes[i] {
+                    //         offset= i;
+                    //         break;
+                    //     }
+                    // }      
+                    
+                    edit_dist(&mut matrix, m, n, current_min as isize, offset, &word_buffer, &target);
+                    
                     let distance = matrix[m][n];
 
-                    if distance == current_min {
-                        closest_words.push(*target);
-                    } else if distance < current_min {
+                    
+                    if distance < current_min {
                         current_min = distance;
                         closest_words.clear();
+                        closest_words.push(*target);
+                    }
+                    else if distance == current_min {
                         closest_words.push(*target);
                     }
 
@@ -185,9 +182,32 @@ fn main() {
                 _ => panic!("non-allowed character!"),
             }),
 
-            b => word_buffer.push(*b),
+            b => word_buffer.push(b),
         }
     }
+}
+
+fn edit_dist(matrix:&mut [[usize;MAX_LENGTH+1];MAX_LENGTH+1],m:usize,n:usize,k:isize,offset:usize,source: &CharArray,target:&CharArray){
+    for i in 1..m + 1 {
+
+        let raisedFloor = max( (i as isize - k), (offset as isize + 1) ) as usize;
+        let loweredCeil = min( (k+i as isize) , (n as isize) ) as usize;
+
+        for j in raisedFloor..=loweredCeil{
+
+            let replace_cost =
+                if source.bytes[(i - 1)] == target.bytes[(j - 1)] {
+                    0
+                } else {
+                    1
+                };
+
+            let ins_del_minimum = min(matrix[(i - 1)][j] + 1, matrix[i][j - 1] + 1);
+            matrix[i][j] =
+                min(ins_del_minimum, matrix[i - 1][j - 1] + replace_cost);
+        }
+    }
+
 }
 
 fn print_matrix(matrix: &Vec<Vec<usize>>) {
@@ -225,3 +245,32 @@ fn pause() {
 }
 
 
+// let mut matrix = [0u8;(40+1)*(40+1)];
+
+    //initialize basecases, i.e the margins
+    // for i in 0..MAX_LENGTH + 1 {
+    //     matrix[i]=i as u8;
+    //     matrix[i*41]=i as u8;
+        
+    // }
+    //print_1d_matrix(&matrix, MAX_LENGTH+1, MAX_LENGTH+1);
+    //println!("{}",get_element(&matrix, 41, 40));
+
+    // for i in 1..m + 1 {
+                    //     for j in 1 + offset..n + 1 {
+
+                    //         let replace_cost =
+                    //             if word_buffer.bytes[(i - 1)] == target.bytes[(j - 1)] {
+                    //                 0
+                    //             } else {
+                    //                 1
+                    //             };
+                            
+                    //         let ins_del_minimum = min(matrix[(i - 1)*41+j] + 1, matrix[i*41+j- 1] + 1);
+                    //         matrix[i*41+j] =
+                    //             min(ins_del_minimum, matrix[(i - 1)*41 + j -1] + replace_cost);
+                    //     }
+                    // }
+                    // // print_1d_matrix(&matrix, m, n);
+                    // // println!();
+                    // let distance = matrix[m*41+n];
